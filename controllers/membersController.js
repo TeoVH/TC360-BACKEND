9b060d4740bc940c5eb4bf796bd91c63c442462a
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-10-04 14:16:38 -0500

    controller

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,29 +1,27 @@
 /* Module */
 
-const User = require('../models/User')
-
+const User = require('../models/User');
 
 /* Route */
 
 const memberList = async (req, res) => {
-    try{
-        let alph = req.body.nickname;
-        let list = await User.find().sort({
-            nickname: 1})
-        const userMap = {};
-        list.forEach((user) => {
-            userMap[user.nickname] = user;
-        })
-        res.send(list);
-    } catch(err){
-        console.log(err)
-    }
+  try {
+    let alph = req.body.nickname;
+    let list = await User.find({}, { nickname: 1, email: 1 }).sort({
+      nickname: 1,
+    });
+    const userMap = {};
+    list.forEach((user) => {
+      userMap[user.nickname] = user;
+    });
+    res.send(list);
+  } catch (err) {
+    console.log(err);
+  }
 };
-
 
 /* Exports */
 
 module.exports = {
-    memberList
+  memberList,
 };
-
